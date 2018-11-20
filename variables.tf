c47dd0107d28ca5b9f2820e09c912a7f39848d3b
hpcdood <38863044+hpcdood@users.noreply.github.com>
2018-11-20 05:00:18 -0500

    upd variables

--- variables.tf
+++ variables.tf
@@ -1,2 +1,13 @@
 # Variables
 variable "time_zone" {}
+variable "aws_region" {
+  description = "EC2 Region for VPC"
+  default = "us-east-2"
+}
+
+variable "amis" {
+  description = "AMI by region"
+  default = {
+    us-east-2 = "ami-03291866"
+  }
+}
